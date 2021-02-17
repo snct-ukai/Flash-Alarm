8fa1c30014fa66333606ebafee405683648c87e5
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-02-17 16:05:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Flash Alarm/MainActivity.cs
@@ -0,0 +1,141 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Plugin.FilePicker;
+using System.IO;
+using System;
+using Android.Util;
+using Android.Text.Format;
+using System.Threading.Tasks;
+using Android.Content;
+
+namespace Flash_Alarm
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public TextView filename { get; private set; }
+        public TextView timeDisplay { get; private set; }
+        public TextView Alarm { get; private set; }
+        public string filepath;
+        public DateTime alarmtime;
+        public bool aswitch = false;
+        public bool timeswitch = false;
+
+        public async void timer()
+        {
+            bool a = true;
+            while (a)
+            {
+                if (aswitch)
+                {
+                    if (alarmtime.ToShortTimeString() == DateTime.Now.ToShortTimeString())
+                    {
+                        var intent = new Intent(this, typeof(secound));
+                        intent.PutExtra("Message", this.filepath);
+                        StartActivity(intent);
+                        a = false;
+                    }
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            timeDisplay = FindViewById<TextView>(Resource.Id.time);
+            filename = FindViewById<TextView>(Resource.Id.filename);
+            Alarm = FindViewById<Button>(Resource.Id.alarmswitch);
+
+            Button TimeSet = FindViewById<Button>(Resource.Id.timebutton);
+            TimeSet.Click += TimeSet_Click;
+
+            Button FileSet = FindViewById<Button>(Resource.Id.filebutton);
+            FileSet.Click += FileSet_Click;
+
+            Button AlarmSwitch = FindViewById<Button>(Resource.Id.@alarmswitch);
+            AlarmSwitch.Click += AlarmSwitch_Click;
+
+            timer();
+        }
+
+        private void AlarmSwitch_Click(object sender, EventArgs e)
+        {
+            if (timeswitch)
+            {
+                aswitch = !aswitch;
+                Alarm.Text = aswitch ? "ON" : "OFF";
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public class TimePickerFragment : DialogFragment, TimePickerDialog.IOnTimeSetListener
+        {
+            public static readonly string TAG = "MyTimePickerFragment";
+            Action<DateTime> timeSelectedHandler = delegate { };
+
+            public static TimePickerFragment NewInstance(Action<DateTime> onTimeSelected)
+            {
+                TimePickerFragment frag = new TimePickerFragment();
+                frag.timeSelectedHandler = onTimeSelected;
+                return frag;
+            }
+
+            public override Dialog OnCreateDialog(Bundle savedInstanceState)
+            {
+                DateTime currentTime = DateTime.Now;
+                bool is24HourFormat = DateFormat.Is24HourFormat(Activity);
+                TimePickerDialog dialog = new TimePickerDialog
+                    (Activity, this, currentTime.Hour, currentTime.Minute, is24HourFormat);
+                return dialog;
+            }
+
+            public void OnTimeSet(TimePicker view, int hourOfDay, int minute)
+            {
+                DateTime currentTime = DateTime.Now;
+                DateTime selectedTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, hourOfDay, minute, 0);
+                Log.Debug(TAG, selectedTime.ToLongTimeString());
+                timeSelectedHandler(selectedTime);
+            }
+        }
+
+        private async void FileSet_Click(object sender, EventArgs e)
+        {
+            var file = await CrossFilePicker.Current.PickFile();
+            if (Path.GetExtension(file.FilePath) == ".mp3")
+            {
+                filepath = file.FilePath;
+                filename.Text = file.FileName;
+            }
+            else
+            {
+                filename.Text = "mp3ファイルを選択してください";
+            }
+        }
+
+        private void TimeSet_Click(object sender, EventArgs e)
+        {
+            TimePickerFragment frag = TimePickerFragment.NewInstance(
+                delegate (DateTime times)
+            {
+                timeDisplay.Text = times.ToShortTimeString();
+                alarmtime = times;
+                timeswitch = true;
+            });
+            frag.Show(FragmentManager, TimePickerFragment.TAG);
+        }
+    }
+}
---
+++ Flash Alarm/secound.cs
@@ -0,0 +1,110 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using System.Threading.Tasks;
+using System;
+using Android.Media;
+
+namespace Flash_Alarm
+{
+    [Activity(Label = "secound")]
+    public class secound : Activity
+    {
+        protected MediaPlayer player;
+        public TextView NumberDisplay { get; private set; }
+        public TextView ButtonDisplay { get; private set; }
+        public TextView Number { get; private set; }
+
+        public bool button = true;
+        public int ans;
+
+        Random rnd = new Random();
+
+        public void StartPlayer(string filePath)
+        {
+            if (player == null)
+            {
+                player = new MediaPlayer();
+                player.SetDataSource(filePath);
+                player.Prepare();
+                player.Start();
+            }
+            else
+            {
+                player.Reset();
+                player.SetDataSource(filePath);
+                player.Prepare();
+                player.Start();
+            }
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.secound);
+
+            ButtonDisplay = FindViewById<Button>(Resource.Id.activity_button);
+            Number = FindViewById<EditText>(Resource.Id.number);
+            NumberDisplay = FindViewById<TextView>(Resource.Id.numberdisplay);
+
+            Button activity_button = FindViewById<Button>(Resource.Id.activity_button);
+            activity_button.Click += Activity_button_Click;
+
+            StartPlayer(this.Intent.GetStringExtra("Message"));
+        }
+
+        private async void Start()
+        {
+            Button activity_button = FindViewById<Button>(Resource.Id.activity_button);
+            activity_button.Enabled = false;
+
+            Number.Text = "";
+
+            int num;
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                num = rnd.Next(1, 10);
+                sum += num;
+                NumberDisplay.Text = num.ToString();
+                await Task.Delay(1000);
+                NumberDisplay.Text = "";
+                await Task.Delay(50);
+            }
+            NumberDisplay.Text = "答えを入力してください";
+            ans = sum;
+
+            activity_button.Enabled = true;
+        }
+
+        private void Judge()
+        {
+            if (Number.Text == ans.ToString())
+            {
+                NumberDisplay.Text = "正解です";
+                player.Reset();
+            }
+            else
+            {
+                NumberDisplay.Text = "それじゃあダメなんよ";
+            }
+        }
+
+        private void Activity_button_Click(object sender, System.EventArgs e)
+        {
+            if (button)
+            {
+                Start();
+                button = false;
+                ButtonDisplay.Text = "入力";
+            }
+            else
+            {
+                Judge();
+                ButtonDisplay.Text = "スタート";
+                button = true;
+            }
+        }
+    }
+}
